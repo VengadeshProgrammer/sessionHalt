52f7052e021b69234f15af57c58a40f519c7b8b9
VengadeshProgrammer <aakashcoder123@gmail.com>
2025-10-14 21:11:29 +0530

    deb: 6

--- api/signup.js
+++ api/signup.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 import { generateSessionId } from "./generateSessionId.js";
 import cookie from "cookie";
-
+console.log(process.env.VITE_SUPABASE_URL, process.env.VITE_SUPABASE_ANON_KEY);
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_ANON_KEY
