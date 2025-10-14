54d74d77905e02d8ef951d8d2eeacd7ce443b9ca
Vengadesh <145902761+VengadeshProgrammer@users.noreply.github.com>
2025-10-15 05:16:30 +0530

    Update signup.js

--- api/signup.js
+++ api/signup.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 import { generateSessionId } from "./generateSessionId.js";
 import cookie from "cookie";
-console.log(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
+console.log(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY, "from line 4 of api/signup.js);
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_ANON_KEY
