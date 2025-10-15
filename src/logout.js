e6a829148f63a7c71bf7ce707f60beacdac23de8
Vengadesh <145902761+VengadeshProgrammer@users.noreply.github.com>
2025-10-15 06:15:47 +0530

    Update logout.js

--- src/logout.js
+++ src/logout.js
@@ -1,5 +1,4 @@
 async function logoutUser() {
-    alert("⚠️ Possible canvas.toDataURL() tampering detected!, Don't try to hack!");
   const res = await fetch("/api/logout", {
       method: "POST",
       credentials: "include", // ✅ sends the cookie (sessionId)
